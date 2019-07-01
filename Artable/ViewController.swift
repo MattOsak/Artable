1d0917d5c5bef8835639417e313a557672255c2d
MattOsak <matt.osak@gmail.com>
2019-07-01 15:49:59 -0400

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Matt Osak on 2019-07-01.
+//  Copyright © 2019 Matt Osak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
